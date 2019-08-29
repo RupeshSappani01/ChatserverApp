803b82c887de607fc34a83db8a38bbcf735d6633
madanreddy1100 <madan@rsosbiz.com>
2019-08-29 17:48:52 +0530

    make it better

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ var socket = require('socket.io');
 // App setup
 var app = express();
 var server = app.listen(process.env.PORT || 4000, function () {
-    console.log('listening for requests on port',process.env.PORT);
+    console.log('listening for requests on port', process.env.PORT);
 });
 var clients = [];
-var CheckUserOnlineID="";
+var DocumentsList=[];
+var CheckUserOnlineID = "";
 // Static files
 app.use(express.static('public'));
 // Socket setup & pass server
-var io = socket(server,{
+var io = socket(server, {
     'pingTimeout': 6000000,
     'pingInterval': 45000
 });
@@ -19,63 +20,74 @@ io.on('connection', (socket) => {
     //console.log('made socket connection', socket.id);
     socket.on('storeClientInfo', function (data) {
         let response;
-        response = {UserID:data.customId,isActive: true};
-        socket.broadcast.emit('onIsActive',response)
+        response = {
+            UserID: data.customId,
+            isActive: true
+        };
+        socket.broadcast.emit('onIsActive', response)
         console.log('CustomerID', data);
         var clientInfo = new Object();
         clientInfo.customId = data.customId;
         clientInfo.clientId = socket.id;
-        clientInfo.UserName=data.username;
+        clientInfo.UserName = data.username;
         clients.push(clientInfo);
         console.log(clients);
-        socket.broadcast.emit('GetOnlineUsers',clients);
+        // for (var i = 0, len = clients.length; i < len; ++i) {
+        //     if(clients[i].customId==data.customId){
+        //         clients.splice(i,1);
+        //     }
+        // }       
+        //  socket.broadcast.emit('GetOnlineUsers', clients);
     });
-//Check User Online Status
-socket.on('isActive', function (userId) {
-    var UserID = userId;
-    var user=null;
-    for (var i = 0, len = clients.length; i < len; ++i) {
-        var c = clients[i];
-        if (c.customId == UserID) {
-            user=c.clientId;
-            break;
-        }else{
-            user = null;
+
+    //Check User Online Status
+    socket.on('isActive', function (userId) {
+        var UserID = userId;
+        var user = null;
+        for (var i = 0, len = clients.length; i < len; ++i) {
+            var c = clients[i];
+            if (c.customId == UserID) {
+                user = c.clientId;
+                break;
+            } else {
+                user = null;
+            }
         }
-    }
-    const socketId = socket.id;
-    let response;
-    if(user) {
-      // User is active
-      response = {UserID:userId,isActive: true};
-    } else {
-      // User is not active
-      response = {UserID:userId,isActive: false};
-    }
-    const responseSocket = io.sockets.connected[socketId];
-    if(responseSocket) {
-        console.log(response)
-      responseSocket.emit('onIsActive', response);
-    }
-  });
+        const socketId = socket.id;
+        let response;
+        if (user) {
+            // User is active
+            response = {
+                UserID: userId,
+                isActive: true
+            };
+        } else {
+            // User is not active
+            response = {
+                UserID: userId,
+                isActive: false
+            };
+        }
+        const responseSocket = io.sockets.connected[socketId];
+        if (responseSocket) {
+            console.log(response)
+            responseSocket.emit('onIsActive', response);
+        }
+    });
     // Handle chat event
     socket.on('chat', function (data) {
         console.log(data);
-
         clients.forEach(function (item, index) {
             if (item.customId == data.customId) {
                 console.log(item.clientId);
                 socket.broadcast.to(item.clientId).emit('chat', data);
             }
         })
-
-
-
     });
-//online Users Get
-socket.on('GetOnlineUsers',function(data){
-socket.emit('GetOnlineUsers',clients);
-})
+    //online Users Get
+    socket.on('GetOnlineUsers', function (data) {
+        socket.emit('GetOnlineUsers', clients);
+    })
     // Handle typing event
     socket.on('typing', function (data) {
         clients.forEach(function (item, index) {
@@ -83,7 +95,7 @@ socket.emit('GetOnlineUsers',clients);
                 console.log(item.clientId);
                 socket.broadcast.to(item.clientId).emit('typing', data);
             }
-        }) 
+        })
     });
     socket.on('disconnect', function (data) {
         console.log("Disconnected ID", socket.id);
@@ -93,13 +105,16 @@ socket.emit('GetOnlineUsers',clients);
             if (c.clientId == socket.id) {
                 clients.splice(i, 1);
                 let response;
-                response = {UserID:c.customId,isActive: false};
-                socket.broadcast.emit('onIsActive',response)
-                socket.broadcast.emit('GetOnlineUsers',clients);
+                response = {
+                    UserID: c.customId,
+                    isActive: false
+                };
+                socket.broadcast.emit('onIsActive', response)
+                socket.broadcast.emit('GetOnlineUsers', clients);
                 break;
             }
         }
-    console.log(clients);
+        console.log(clients);
     });
 
 });
--- public/chat.js
+++ public/chat.js
@@ -1,23 +1,26 @@
 // Make connection
 var urlParams = new URLSearchParams(window.location.search);
-var getID=urlParams.get('ID');
-var getUserName=urlParams.get('Name');
-let socket = io.connect('https://chatserverforapps.herokuapp.com/');
+var getID = urlParams.get('ID');
+var getUserName = urlParams.get('Name');
+let socket = io.connect('http://localhost:4000/');
 socket.on('connect', function (data) {
     var CustomerID = getID;
-    var UserName=getUserName
+    var UserName = getUserName
     console.log(CustomerID);
-    socket.emit('storeClientInfo', { customId: CustomerID,username:UserName });
+    socket.emit('storeClientInfo', {
+        customId: CustomerID,
+        username: UserName
+    });
 });
 // Query DOM
 var message = document.getElementById('message'),
     handle = document.getElementById('handle'),
     btn = document.getElementById('send'),
     GetID = document.getElementById('GetID');
-    AddID = document.getElementById('AddID');
-    ShowTheFirst = document.getElementById('ShowTheFirst');
-    DontShowFirst = document.getElementById('DontShowFirst');
-    CustomerID1111 = document.getElementById('CustomerID'),
+AddID = document.getElementById('AddID');
+ShowTheFirst = document.getElementById('ShowTheFirst');
+DontShowFirst = document.getElementById('DontShowFirst');
+CustomerID1111 = document.getElementById('CustomerID'),
     output = document.getElementById('output'),
     feedback = document.getElementById('feedback');
 // Emit events
@@ -26,18 +29,17 @@ btn.addEventListener('click', function () {
         message: message.value,
         handle: getUserName,
         customId: ClickedID,
-        MyID:getID
+        MyID: getID
     });
 
-    output.innerHTML += '<div id="YourMsg">'+message.value+'</div>';
+    output.innerHTML += '<div id="YourMsg">' + message.value + '</div>';
     message.value = "";
-
 });
 message.addEventListener('keypress', function () {
     socket.emit('typing', {
-        TyperName:getUserName,
-        customId:ClickedID,
-        MyID:getID
+        TyperName: getUserName,
+        customId: ClickedID,
+        MyID: getID
     });
 })
 
@@ -47,72 +49,60 @@ message.addEventListener('keypress', function () {
 
 // Listen for events
 socket.on('chat', function (data) {
-  if(data.MyID==ClickedID){
+    if (data.MyID == ClickedID) {
         feedback.innerHTML = '';
         output.innerHTML += ' <div id="OthersMsg">' + data.message + '</div>';
-}
-});
-
-socket.on('typing', function (data) {
-    if(data.MyID==ClickedID){
-    feedback.innerHTML = '<p><em>' + data.TyperName + ' is typing a message...</em></p>';
     }
 });
 
-// socket.on('onIsActive', function (data) {
-//     console.log('aaaaa', data);
-//     if(data.UserID=3)
-//     if (data.isActive == true) {
-//         document.getElementById('IsActive').innerHTML = "online";
-//         document.getElementById('IsActive').style.color = 'green';
-//     } else {
-//         document.getElementById('IsActive').innerHTML = "Offline";
-//         document.getElementById('IsActive').style.color = 'red';
-//     }
+socket.on('typing', function (data) {
+    if (data.MyID == ClickedID) {
+        feedback.innerHTML = '<p><em>' + data.TyperName + ' is typing a message...</em></p>';
+    }
+});
 
-// })
 var ClickedID;
 var ClickedName;
-function OpenChatAccording(item,index){
-   $('#output').empty();
-   $('#feedback').empty();
-    ClickedID=item;
-    ClickedName=$('.oneoneuser'+index+'').attr('data-UserName');
+
+function OpenChatAccording(item, index) {
+    $('#output').empty();
+    $('#feedback').empty();
+    ClickedID = item;
+    ClickedName = $('.oneoneuser' + index + '').attr('data-UserName');
     $('#UserNameShoe').text(ClickedName);
     $('#myModal').modal('show');
     socket.emit('isActive', ClickedID)
 }
 GEtAllUsers();
-function GEtAllUsers(){
-    socket.emit('GetOnlineUsers','');
-}
-socket.on('GetOnlineUsers',function(data){
 
-    var tr='';
-    if(data.lenght!=0){
-        $.each(data,function(index,item){
-            if(item.customId!=getID){
-                tr+='<div id="oneoneuser" class="oneoneuser'+index+'" data-UserName='+item.UserName+' onclick="OpenChatAccording('+item.customId+','+index+')">';
-                tr +='<h6><span id="YourOnline">'+index+'</span>&nbsp;&nbsp;&nbsp;&nbsp;'+item.UserName+'</h6>'
-                tr+=' </div>' ;  
-            } 
+function GEtAllUsers() {
+    socket.emit('GetOnlineUsers', '');
+}
+socket.on('GetOnlineUsers', function (data) {
+    console.log(data);
+    var tr = '';
+    if (data.length != 0) {
+        $.each(data, function (index, item) {
+            if (item.customId != getID) {
+                tr += '<div id="oneoneuser" class="oneoneuser' + index + '" data-UserName=' + item.UserName + ' onclick="OpenChatAccording(' + item.customId + ',' + index + ')">';
+                tr += '<h6><span id="YourOnline">' + index + '</span>&nbsp;&nbsp;&nbsp;&nbsp;' + item.UserName + '</h6>'
+                tr += ' </div>';
+            }
         })
-    }else{
-        tr +='<div style="color:red">No Users In Online</div>';
+    } else {
+        tr += '<div style="color:red">No Users In Online</div>';
     }
-   
     $('#ChatShowDiv').empty().append(tr);
-          
 })
 
 socket.on('onIsActive', function (data) {
-    if(ClickedID=data.UserID){
+    if (ClickedID = data.UserID) {
         if (data.isActive == true) {
-           $('#ShowWhenColor').text('Online');
-           $('#ShowWhenColor').css('color','green');
-        }else{
+            $('#ShowWhenColor').text('Online');
+            $('#ShowWhenColor').css('color', 'green');
+        } else {
             $('#ShowWhenColor').text('Offline');
-            $('#ShowWhenColor').css('color','red');
+            $('#ShowWhenColor').css('color', 'red');
         }
     }
 })
