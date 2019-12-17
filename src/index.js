e7d232ddf0190477f39b7ee08d8f2803302f1306
madanreddy1100 <madan@rsosbiz.com>
2019-12-17 15:37:14 +0530

    make it better

--- src/index.js
+++ src/index.js
@@ -31,6 +31,7 @@ const nsp=io.of('/Register');
 nsp.on('connection',(socket)=>{
     console.log("connected",socket.id);
     socket.on("RegistedSuccess",function(data){
+        console.log("Got The Message",data);
         socket.broadcast.emit("RegistedSuccess",data);
  })
 })
