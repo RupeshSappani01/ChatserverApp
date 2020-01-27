1e94f8bbc30a0fd7d9fa61d5fc85aae53d2d83f4
madanreddy1100 <madan@rsosbiz.com>
2020-01-27 15:27:18 +0530

    make it better

--- src/index.js
+++ src/index.js
@@ -59,7 +59,12 @@ nsp1.on('connection', (socket) => {
         clientInfo.customId = data.customId;
         clientInfo.clientId = socket.id;
         clientInfo.UserName = data.username;
-        clients.push(clientInfo);
+        for(var i = 0, len = clients.length; i < len; ++i){
+            if(clients[i].clientId!=clientInfo.clientId){
+                clients.push(clientInfo);
+            }
+        }
+       
         console.log(clients);
         // for (var i = 0, len = clients.length; i < len; ++i) {
         //     if(clients[i].customId==data.customId){
